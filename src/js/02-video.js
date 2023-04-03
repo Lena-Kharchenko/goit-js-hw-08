138de9738281867efa062f592b95c476485e9a8d
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-04-03 19:09:41 +0300

    update task2.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(dataTimeUpdate => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      `${dataTimeUpdate.seconds}`
+    );
+  }, 1000)
+);
