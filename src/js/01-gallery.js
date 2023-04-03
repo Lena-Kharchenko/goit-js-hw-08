da6cf1f5d42c845bdcc69897a1071fa8dbbd2176
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-04-03 18:38:01 +0300

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const listItem = document.querySelector('.gallery');
+
+const imgList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+  })
+  .join(' ');
+
+listItem.insertAdjacentHTML('afterbegin', imgList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+// import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// // Change code below this line
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const galeryMarkUp = galleryItems
+//   .map(({ description, original, preview }) => {
+//     return `
+//         <a class="gallery__item" href="${original}">
+//             <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+//         </a>
+//     `;
+//   })
+//   .join(' ');
+
+// galleryEl.insertAdjacentHTML('afterbegin', galeryMarkUp);
+
+// var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
