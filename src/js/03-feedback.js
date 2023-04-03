63065f56af97d619d98eb6222dcd300d7ff05c29
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-04-03 19:56:20 +0300

    update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'feedback-form-state';
+
+feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', throttle(onInputData, 500));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+const { email, message } = feedbackForm.elements;
+reloadPage();
+
+function onInputData(evt) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть поля форми, будь ласка');
+  }
+
+  localStorage.removeItem(LOCAL_KEY);
+  evt.currentTarget.reset();
+  dataForm = {};
+}
